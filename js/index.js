94c78f6e5277ce3a291cacb516f0cb4e082ff3f2
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2019-02-16 21:59:32 +0100

    Reduzco tiempo vibración

--- js/index.js
+++ js/index.js
@@ -108,7 +108,7 @@ waterButton.addEventListener("mousedown", function(){chooseElement(this)}, false
 
 function chooseElement (elementObject) {
 	
-	window.navigator.vibrate(50);
+	window.navigator.vibrate(25);
 
 	if(!activeButton && gameActive){
 		activeButton = true;
