e4eef57abd36a34ede2467bd982b7532b8b25ea3
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2020-08-11 13:09:33 +0200

    Corrijo sw

--- workbox-config.js
+++ workbox-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   "globDirectory": ".",
   "globPatterns": [
-    "**/*.{css,eot,svg,ttf,woff,js,jpg,png,html,json,md,scss}"
+    "**/*.{css,eot,svg,ttf,woff,js,jpg,png,html,json}"
   ],
   "swDest": "./sw.js",
   "swSrc": "src-sw.js"
