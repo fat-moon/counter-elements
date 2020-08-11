0e86f63a97847d11ee957a3540512d4185c78b98
Juan Antonio Ledesma <juanantonioledesma@gmail.com>
2020-08-11 12:09:44 +0200

    Genero sw

--- src-sw.js
+++ src-sw.js
@@ -1,3 +1,5 @@
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
+// importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
-workbox.precaching.precacheAndRoute([]);
+import {precacheAndRoute} from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
